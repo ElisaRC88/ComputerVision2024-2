c95d3f455e46e4c4ec90ab305a1753e2efb80da2
ElisaRC88 <erendonc@eafit.edu.co>
2024-07-25 12:38:24 -0500

    actualizado

--- Semana1/Ejercicio1.py
+++ Semana1/Ejercicio1.py
@@ -1,5 +1,23 @@
+#%%
 import numpy as np
 import cv2
+import PIL
+from PIL import Image, ImageDraw
 
-im = np.zeros((100,100), np.uint8)
-im
+#%%
+def show_im(im):
+    cv2.imshow('window', im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#%%
+im1 = np.zeros((100,100), np.uint8)
+show_im(im1)
+
+#%%
+for i in range(100): im1[i,i]=255
+show_im(im1)
+
+#%%
+
+
